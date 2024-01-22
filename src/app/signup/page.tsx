5ec43ad9f3884841441c2738235474a11aaf1ba7
Himanshu <himanshujain5690174@gmail.com>
2024-01-22 20:50:11 +0530

    Setting-up todo project

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+function Signup() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-10/12 flex justify-start items-center">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
